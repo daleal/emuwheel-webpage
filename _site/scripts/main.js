66b3376006a118d2d89610dd65c19ca90a4aaafc
Daniel Leal <dlleal@uc.cl>
2018-11-24 18:03:01 -0300

    Menu shop shows only in desktop mode

--- _site/scripts/main.js
+++ _site/scripts/main.js
@@ -1,14 +1,21 @@
 $(function() {
     $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 700);
-          return false;
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 700);
+                return false;
+            }
         }
-      }
     });
-  });
+});
+
+
+jQuery(window).load(function(){
+    if (jQuery(window).width() <= 800){
+        jQuery(".hero-actions a").attr("href", "/la-rueda")
+    }
+});
